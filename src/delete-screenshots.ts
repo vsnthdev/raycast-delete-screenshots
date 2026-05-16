d5a225322f10878c72869ade2f9163925fb55fdd
Vasanth Srivatsa <vsnthdev@gmail.com>
2026-05-16 18:22:48 +0530

    🎊 deletes screenshots by full page screencapture extension in Chrome

--- src/delete-screenshots.ts
+++ src/delete-screenshots.ts
@@ -27,6 +27,7 @@ export default async function main() {
     const promises: Promise<string[]>[] = [
         glob(join(preferences.folder, 'Screenshot [0-9][0-9][0-9][0-9]*.png')),
         glob(join(preferences.folder, 'Screenshot_[0-9][0-9][0-9][0-9]*.jpg')),
+        glob(join(preferences.folder, 'screencapture-*-[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]_[0-9][0-9].jpg')),
     ]
 
     if (preferences.deleteWhatsAppImages) {
