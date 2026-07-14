49e515ffec1cc86c70cccc26feba979ae95b6dfa
Vasanth Srivatsa <vsnthdev@gmail.com>
2026-07-14 20:06:50 +0530

    🐛 fix doesn't delete screenshots from Android

--- src/delete-screenshots.ts
+++ src/delete-screenshots.ts
@@ -3,65 +3,80 @@
  *  Created On 04 June 2025
  */
 
-import { showToast, Toast, getPreferenceValues, closeMainWindow } from '@raycast/api'
-import { promises as fs } from 'fs'
-import { join } from 'path'
-import commaNumber from 'comma-number'
-import { glob } from 'glob'
+import {
+  showToast,
+  Toast,
+  getPreferenceValues,
+  closeMainWindow,
+} from "@raycast/api";
+import { promises as fs } from "fs";
+import { join } from "path";
+import commaNumber from "comma-number";
+import { glob } from "glob";
 
 interface Preferences {
-    folder: string
-    deleteWhatsAppImages: boolean
-    deleteRecordings: boolean
+  folder: string;
+  deleteWhatsAppImages: boolean;
+  deleteRecordings: boolean;
 }
 
 export default async function main() {
-    closeMainWindow()
-    const preferences = getPreferenceValues<Preferences>()
+  closeMainWindow();
+  const preferences = getPreferenceValues<Preferences>();
 
+  await showToast({
+    style: Toast.Style.Animated,
+    title: "Scanning for screenshots...",
+  });
+
+  const promises: Promise<string[]>[] = [
+    glob(join(preferences.folder, "Screenshot [0-9][0-9][0-9][0-9]*.png")),
+    glob(join(preferences.folder, "Screenshot_[0-9][0-9][0-9][0-9]*.png")),
+    glob(join(preferences.folder, "Screenshot_[0-9][0-9][0-9][0-9]*.jpg")),
+    glob(
+      join(
+        preferences.folder,
+        "screencapture-*-[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]_[0-9][0-9].jpg",
+      ),
+    ),
+  ];
+
+  if (preferences.deleteWhatsAppImages) {
+    promises.push(
+      glob(
+        join(preferences.folder, "WhatsApp Image [0-9][0-9][0-9][0-9]*.jpeg"),
+      ),
+    );
+  }
+
+  if (preferences.deleteRecordings) {
+    promises.push(
+      glob(
+        join(preferences.folder, "Screen Recording [0-9][0-9][0-9][0-9]*.mov"),
+      ),
+    );
+  }
+
+  const nested = await Promise.all(promises);
+  const files = nested.flat(1);
+
+  if (files.length == 0) {
     await showToast({
-        style: Toast.Style.Animated,
-        title: 'Scanning for screenshots...',
-    })
+      style: Toast.Style.Success,
+      title: `Deleted ${commaNumber(0)} screenshots`,
+    });
+    return;
+  }
 
-    const promises: Promise<string[]>[] = [
-        glob(join(preferences.folder, 'Screenshot [0-9][0-9][0-9][0-9]*.png')),
-        glob(join(preferences.folder, 'Screenshot_[0-9][0-9][0-9][0-9]*.jpg')),
-        glob(join(preferences.folder, 'screencapture-*-[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]-[0-9][0-9]_[0-9][0-9]_[0-9][0-9].jpg')),
-    ]
+  // delete each png file
+  const deletePromises = files.map(async (file) => {
+    await fs.unlink(file);
+    return file;
+  });
 
-    if (preferences.deleteWhatsAppImages) {
-        promises.push(
-            glob(join(preferences.folder, 'WhatsApp Image [0-9][0-9][0-9][0-9]*.jpeg'))
-        )
-    }
-
-    if (preferences.deleteRecordings) {
-        promises.push(
-            glob(join(preferences.folder, 'Screen Recording [0-9][0-9][0-9][0-9]*.mov'))
-        )
-    }
-
-    const nested = await Promise.all(promises)
-    const files = nested.flat(1)
-
-    if (files.length == 0) {
-        await showToast({
-            style: Toast.Style.Success,
-            title: `Deleted ${commaNumber(0)} screenshots`
-        })
-        return
-    }
-
-    // delete each png file
-    const deletePromises = files.map(async (file) => {
-        await fs.unlink(file)
-        return file
-    })
-
-    const deletedFiles = await Promise.all(deletePromises)
-    await showToast({
-        style: Toast.Style.Success,
-        title: `Deleted ${commaNumber(deletedFiles.length)} screenshots`
-    })
+  const deletedFiles = await Promise.all(deletePromises);
+  await showToast({
+    style: Toast.Style.Success,
+    title: `Deleted ${commaNumber(deletedFiles.length)} screenshots`,
+  });
 }
